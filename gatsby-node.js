7fb7b4d3e44a70f06c413fa09f980e78b88bb6bd
Stephen L <lrq3000@gmail.com>
2021-07-09 05:33:16 +0200

    attempt to create categories pages dynamically
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,77 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+
+// Trick to implement ESM6 import format in gatsby-node.js
+// See: https://github.com/gatsbyjs/gatsby/issues/7810#issuecomment-449741977
+//require = require('esm')(module)
+//module.exports = require('./gatsby-node.esm.js')
+
+// import {getData, withPrefix} from './src/utils'; // we need ESM6 import style but they do not work yet in gatsby-node.js so we'll do without getData, see https://github.com/gatsbyjs/gatsby/issues/7810#issuecomment-449741977
+
+const path = require('path')
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  
+  // Auto-generate category listing pages
+  const { createPage } = actions
+  const thisTemplate = path.resolve("./src/templates/categories_list.js")
+  const response = await graphql(`
+    query {
+      allMarkdownRemark(filter: {frontmatter: {template: {eq: "post"}}}) {
+        edges {
+          node {
+            frontmatter {
+              categories
+            }
+          }
+        }
+        distinct(field: frontmatter___categories)
+      }
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      allSitePage {
+        nodes {
+          path
+        }
+      }
+    }
+  `)
+
+  let siteMetadata = response.data.site.siteMetadata;  // Layout component expects that to feed Helmet
+  let pages = response.data.allSitePage.nodes.map(node => ({'url': node.path}));  // simply get all pages on the whole website, necessary for the utils.getPages() function to work (it will filter for the appropriate section, e.g., blog/)
+  response.data.allMarkdownRemark.distinct.forEach(categoryURL => {
+    // 0. Using GraphQL distinct, we extract the unique list of categories URLs from blog posts markdown files directly
+    // 1. Get category data from categoryURL (for the moment we just infer from URL without getting the data)
+    // 2. Get markdown data
+    // 3. Create new pages
+    // 4. Pass as context the category URL as defined in the markdown post and categoryPseudoID so that the template can handle it
+
+    // Fetch infos about the linked category from its .yaml file
+    // Since we cannot import utils.getData(), we simplify by getting the last segment of the URL (and remove file extension) as the category id, which holds true since the category id == slug == filename (as set in NetlifyCMS)
+    //let category_data = getData(null, categoryURL);
+    let categoryPseudoID = categoryURL.match(/([^\/]*)\/*$/)[1].split('.')[0];
+    //console.log('/blog/category2/' + categoryPseudoID);  // debug
+
+    createPage({
+      component: thisTemplate,
+      path: '/blog/category2/' + categoryPseudoID,  // `${category}`
+      context: {
+        slug: categoryPseudoID,
+        categoryURL: categoryURL,
+        categoryPseudoID: categoryPseudoID,
+        frontmatter: {
+            title: categoryPseudoID,
+            template: `post`,
+        },
+        site: {
+            siteMetadata: siteMetadata,
+        },
+        pages: pages,
+      },
+    })
+  })
+}
--- src/components/BlogPostCategories.js
+++ src/components/BlogPostCategories.js
@@ -3,6 +3,8 @@ import _ from 'lodash';
 
 import {getData, Link, withPrefix} from '../utils';
 
+const path = require('path');
+
 export default class BlogPostCategories extends React.Component {
     render() {
         let categories = _.get(this.props, 'categories', null);
@@ -14,10 +16,8 @@ export default class BlogPostCategories extends React.Component {
             	_.map(categories, (category, category_idx) => {
             	    let category_data = getData(this.props.pageContext.site.data, category);
             	    return (
-                		category_data.link ? (<React.Fragment key={category_idx + '.1'}>
-                			<Link key={category_idx} to={withPrefix(category_data.link)}>{category_data.title}</Link>{(!(category_idx === category_len - 1)) && (', ')}
-                		</React.Fragment>) : <React.Fragment key={category_idx + '.3'}>
-                			<span key={category_idx + '.2'}>{category_data.title}</span>{(!(category_idx === category_len - 1)) && (', ')}
+                        <React.Fragment key={category_idx + '.1'}>
+                			<Link key={category_idx} to={withPrefix(path.join('blog', 'category', category_data.id))}>{category_data.title}</Link>{(!(category_idx === category_len - 1)) && (', ')}
                 		</React.Fragment>
                 	)
             	})}
